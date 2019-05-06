12ef287118ade50ed0ee4950eaabcd19113c4d03
zlucifer <dekxter000@gmail.com>
2019-05-06 20:32:14 +0700

    Create troll.sh

---
+++ troll.sh
@@ -0,0 +1,198 @@
+#!/bin/bash
+#///////////////////////////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////////
+#////                       _            _  __                              ////
+#////                      | |          (_)/ _|                             ////
+#////                   ___| |_   _  ___ _| |_ ___ _ __                     ////
+#////                  |_  / | | | |/ __| |  _/ _ \ '__|                    ////
+#////                   / /| | |_| | (__| | ||  __/ |                       ////
+#////                  /___|_|\__,_|\___|_|_| \___|_|                       ////
+#////                                                                       ////
+#///////////////////////////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////////
+troll(){
+    clear
+    sleep 0.03
+    echo "         _ "
+    sleep 0.03
+    echo "        /_)  _.-===-._ "
+    sleep 0.03
+    echo "       /  .; '_-.'- .'.     _ "
+    sleep 0.03
+    echo "  __ _/  /.'\ ._  '. , \   (_\ "
+    sleep 0.03
+    echo " /\_( \  |.'.-._'- .  '|     \ "
+    sleep 0.03
+    echo " |_)  /  |/'    ''--''\|     _\__ "
+    sleep 0.03
+    echo " \_)_|  /'  ._.   ._.  '\   / )(_\ "
+    sleep 0.03
+    echo " )/  (  \_      _      _/   \' (_| "
+    sleep 0.03
+    echo " /   /    \           /     _) (_/ "
+    sleep 0.03
+    echo " |   '',_ _'._'---'_.'_ _,.'    | \ "
+    sleep 0.03
+    echo " \    /  \ \  '''''  / / /     /   \ "
+    sleep 0.03
+    echo "  \_.'    \ \       / /  |   .' "
+    sleep 0.03
+    echo "   '--.,   \ \     / /  _;-'' "
+    sleep 0.03
+    echo "       '|   \ \   / /   | "
+    sleep 0.03
+    echo "       .;----'''''''----;. "
+    sleep 0.03
+    echo "      /(                 )\ "
+    sleep 0.03
+    echo "      \_''-------------''_/ "
+    sleep 0.03
+    echo "      | '';-----;-----;'' | "
+    sleep 0.03
+    echo "      | / : \ / : \ / : \ | "
+    sleep 0.03
+    echo "      |V.','.V.','.V.','.V| "
+    sleep 0.03
+    echo "      \'./_\.;./_\.;./_\.'/ "
+    sleep 0.03
+    echo "       ';..___________..;' "
+    sleep 0.03
+    echo " ============================="
+    sleep 2
+    echo " ==      Troll Project      =="
+    sleep 0.7
+    echo " ============================="
+}
+load(){
+    echo -e "\n"
+    bar=" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+    barlength=${#bar}
+    i=0
+    while((i<100)); do
+        n=$((i*barlength / 100))
+        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
+        ((i += RANDOM%5+2))
+        sleep 0.2
+    done
+}
+get_sms=$(curl -s http://zlucifer.com/api/sms.php)
+get_call=$(curl -s http://zlucifer.com/api/call.php)
+mulai (){
+    echo "Gunakan Troll Project Lagi?"
+    echo "y/n?"
+    echo
+    read lagi
+    if [ $lagi = "y" ]; then
+        spam
+    else
+        echo "Terimakasih sudah menggunakan Troll project"
+    fi
+}
+#spam
+spam(){
+    clear
+    troll
+    echo
+    echo "[1] Spam SMS"
+    echo "[2] Spam Telp"
+    echo "[3] Close Troll Project"
+    echo
+    echo "1/2/3?"
+    read pilih
+    if [ $pilih = "1" ]; then
+        echo "Troll Spam SMS"
+        #function spam
+        echo
+            echo "Silahkan masukan nomor telp target"
+            echo contoh 0812345678
+           read target # masukin no telp
+           echo
+            echo "Berapa sms yang mau dikirim?"
+            read paket
+            echo
+            echo Apakah nomor $target "dan SMS dikirim "$paket" sudah benar?"
+            echo y/n?
+            read confirm
+            echo
+            if [ $confirm = "y" ]; then
+                load
+                clear
+                echo Melakukan spam SMS ke nomor $target
+                echo
+                    echo "Jangan close aplikasi sebelum spam selesai"            
+                    echo "========================================"
+                    target_do=$get_sms'/sms.php?nomor='$target'&paket='$paket
+                    CURL_RESPONSE=`curl -s -o /dev/null -w "%{http_code}" $target_do`
+                    echo " Gunakan tools dengan bijak"
+                    echo
+                    echo " Love u always "
+                    echo " -zLucifer"
+                    echo "======================================="
+            else
+                echo "Kesalahan"
+            fi
+        mulai
+    elif [ $pilih = "2" ]; then
+        echo "Troll Spam Call"
+        #function spam
+        echo
+        echo "Silahkan masukan nomor telp target"
+            echo contoh 0812345678
+        read target # masukin no telp
+            echo
+            echo Apakah nomor $target "sudah benar?"
+            echo y/n?
+            read confirm
+            echo
+            if [ $confirm = "y" ]; then
+            load
+            clear
+                echo Melakukan spam call ke nomor $target
+            echo
+            echo "Jangan close aplikasi sebelum spam selesai"
+            echo "========================================"
+                cek_target=`curl -s $get_call/call.php?nomor=$target`
+                echo -e $cek_target
+            echo " Gunakan tools dengan bijak"
+            echo
+            echo " Love u always "
+            echo " -zLucifer"
+            echo "========================================"
+            else
+                echo Kesalahan, silahkan coba lagi
+            fi
+        mulai
+    elif [ $pilih = "3" ]; then
+        echo "Terimakasih sudah menggunakan Troll project"
+        close
+    else
+        clear
+        echo "Kesalahan"
+        mulai
+    fi
+}
+close(){
+    exit
+}
+clear
+echo "Loading.."
+load
+clear
+troll
+echo Selamat datang kak, Siapa nick kaka? #tulisan keluar
+read nick #membaca yang ditulis
+clear
+echo "Loading.."
+load
+clear
+troll
+echo Selamat datang $nick ":)"
+echo
+echo "Mulai troll project?"
+echo "y/n?"
+read mulai
+if [ $mulai = "y" ]; then
+    spam
+else
+    close
+fi
